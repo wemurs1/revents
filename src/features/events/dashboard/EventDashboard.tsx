87be23d93883875b780baa5fcf2540974a129c2f
Stuart Murray <stuart.murray.dev@outlook.com>
2023-10-09 13:57:40 +0100

    add useEffect to load sample data

--- src/features/events/dashboard/EventDashboard.tsx
+++ src/features/events/dashboard/EventDashboard.tsx
@@ -1,6 +1,8 @@
 import { Grid } from 'semantic-ui-react';
 import EventList from './EventList';
 import EventForm from '../form/EventForm';
+import { useEffect, useState } from 'react';
+import { AppEvent } from '../../../app/types/event';
 import { sampleData } from '../../../app/api/sampleData';
 
 type Props = {
@@ -9,10 +11,16 @@ type Props = {
 };
 
 export default function EventDashboard({ formOpen, setFormOpen }: Props) {
+  const [events, setEvents] = useState<AppEvent[]>([]);
+
+  useEffect(() => {
+    setEvents(sampleData);
+  }, []);
+
   return (
     <Grid>
       <Grid.Column width={10}>
-        <EventList events={sampleData} />
+        <EventList events={events} />
       </Grid.Column>
       <Grid.Column width={6}>
         {formOpen && <EventForm setFormOpen={setFormOpen} />}
