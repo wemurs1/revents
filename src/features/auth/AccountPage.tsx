9d027093566fe4ab7dc67c39664a8906665e00ca
Stuart Murray <stuart.murray.dev@outlook.com>
2023-10-13 16:45:30 +0100

    change password at firebase

--- src/features/auth/AccountPage.tsx
+++ src/features/auth/AccountPage.tsx
@@ -1,18 +1,41 @@
 import { FieldValues, useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
-import { Button, Form, Header, Icon, Segment } from 'semantic-ui-react';
+import { Button, Form, Header, Icon, Label, Segment } from 'semantic-ui-react';
 import { useAppSelector } from '../../app/store/store';
+import { useEffect } from 'react';
+import { auth } from '../../app/config/firebase';
+import { updatePassword } from 'firebase/auth';
+import { toast } from 'react-toastify';
 
 export default function AccountPage() {
   const {
     register,
     handleSubmit,
+    getValues,
+    watch,
+    trigger,
+    setError,
+    reset,
     formState: { errors, isSubmitting, isValid },
   } = useForm({ mode: 'onTouched' });
   const { currentUser } = useAppSelector((state) => state.auth);
+  const password1 = watch('password1');
+  const password2 = watch('password2');
 
-  function onSubmit(data: FieldValues) {
-    console.log(data);
+  useEffect(() => {
+    if (password2) trigger('password2');
+  }, [password2, trigger, password1]);
+
+  async function onSubmit(data: FieldValues) {
+    try {
+      if (auth.currentUser) {
+        await updatePassword(auth.currentUser, data.password1);
+        toast.success('Password updated successfully');
+        reset();
+      }
+    } catch (error: any) {
+      setError('root.serverError', { type: '400', message: error.message });
+    }
   }
 
   return (
@@ -34,9 +57,28 @@ export default function AccountPage() {
               type='password'
               defaultValue=''
               placeholder='Confirm Password'
-              {...register('password2', { required: true })}
-              error={errors.password2 && 'Confirm Password is required'}
+              {...register('password2', {
+                required: true,
+                validate: {
+                  passwordMatch: (value) =>
+                    value === getValues().password1 || 'Passwords do not match',
+                },
+              })}
+              error={
+                (errors.password2?.type === 'required' &&
+                  'Confirm Password is required') ||
+                (errors.password2?.type === 'passwordMatch' &&
+                  errors.password2.message)
+              }
             />
+            {errors.root && (
+              <Label
+                basic
+                color='red'
+                style={{ display: 'block', marginBottom: 10 }}
+                content={errors.root.serverError.message}
+              />
+            )}
             <Button
               loading={isSubmitting}
               type='submit'
