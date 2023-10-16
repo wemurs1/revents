3681251628bd9948addccb0bcff3560f541f869a
Stuart Murray <stuart.murray.dev@outlook.com>
2023-10-16 17:17:13 +0100

    clean version - still broken

--- src/features/events/dashboard/EventDashboard.tsx
+++ src/features/events/dashboard/EventDashboard.tsx
@@ -1,25 +1,21 @@
 import { Grid } from 'semantic-ui-react';
 import EventList from './EventList';
 import { useAppSelector } from '../../../app/store/store';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { actions } from '../eventSlice';
 import LoadingComponent from '../../../app/layout/LoadingComponent';
 import { useFireStore } from '../../../app/hooks/firestore/useFireStore';
 import EventFilters from './EventFilters';
-import { QueryOptions } from '../../../app/hooks/firestore/types';
 
 export default function EventDashboard() {
   const { data: events, status } = useAppSelector((state) => state.events);
   const { loadCollection } = useFireStore('events');
-  const [query, setQuery] = useState<QueryOptions[]>([
-    { attribute: 'date', operator: '>=', value: new Date() }
-  ])
 
   useEffect(() => {
     loadCollection(actions, {
-      queries: query
-    })
-  }, [loadCollection, query])
+      queries: [{ attribute: 'date', operator: '>=', value: new Date() }],
+    });
+  }, [loadCollection]);
 
   if (status === 'loading') return <LoadingComponent />;
 
